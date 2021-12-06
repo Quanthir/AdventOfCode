1b861bfd3a089ac493acc676477102afdf78d06a
Gökhan Öztürk <gokhan@eg-ozturk.com>
2021-12-06 14:10:13 +0300

    Day 5 solution

--- 2021/05.py
+++ 2021/05.py
@@ -1,67 +1,67 @@
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
+Line = namedtuple('Line', ['Start', 'End'])
+
 data = ""
 
 with open('2021/05.txt') as f:
     data = f.read()
 
-class Coord:
-    start_x: int
-    start_y: int
-    end_x: int
-    end_y: int
+data = [
+    [
+        [int(x) for x in row.split(',')]
+        for row in line.split(' -> ')
+    ]
+    for line in data.split("\n")
+]
 
-    def __init__(self, s: str):
-        start, end = s.split(' -> ')
-        x1, y1 = map(int, start.split(','))
-        x2, y2 = map(int, end.split(','))
+data = [Line(Point(*row[0]), Point(*row[1])) for row in data]
 
-        self.start_x = x1 if x1 < x2 else x2
-        self.start_y = y1 if y1 < y2 else y2
-        self.end_x = x1 if x1 > x2 else x2
-        self.end_y = y1 if y1 > y2 else y2
+def generate_points(line: Line, horizontal=False) -> Point:
+    if line.Start == line.End:
+        yield line.Start
+        return
     
-    @property
-    def is_horizontal(self) -> bool:
-        return self.start_x != self.end_x
+    dx = abs(line.End.x - line.Start.x)
+    dy = abs(line.End.y - line.Start.y)
+    minx = min(line.Start.x, line.End.x)
+    miny = min(line.Start.y, line.End.y)
+
+    if not (dx != 0 and dy != 0):
+        for i in range(minx, minx + dx + 1):
+            for j in range(miny, miny + dy + 1):
+                yield Point(i, j)
     
-    @property
-    def is_vertical(self) -> bool:
-        return self.start_y != self.end_y
+    if horizontal and dx == dy:
+        yield line.Start
+        x = line.Start.x
+        y = line.Start.y
+        while x != line.End.x:
+            if line.End.x > line.Start.x:
+                x += 1
+            else:
+                x -= 1
+            
+            if line.End.y > line.Start.y:
+                y += 1
+            else:
+                y -= 1
+            
+            yield Point(x, y)
+
+def solution1(horizontal = False) -> int:
+    matrix = {}
+    count = 0
+    for line in data:
+        for point in generate_points(line, horizontal):
+            matrix.setdefault(point, 0)
+
+            matrix[point] += 1
+            if matrix[point] == 2:
+                count += 1
     
-    @property
-    def is_diagonal(self) -> bool:
-        return abs(self.end_x - self.start_x) == abs(self.end_y - self.start_y)
+    return count
 
-class Matrix:
-    m: list
-    
-    def __init__(self):
-        self.m = []
-
-    def expand(self, c: Coord):
-        while len(self.m) <= c.end_y:
-            self.m.append([])
-        
-        for y in range(c.start_y, c.end_y + 1):
-            while len(self.m[y]) <= c.end_x:
-                self.m[y].append(0)
-    
-
-data = [Coord(line) for line in data.split("\n")]
-
-def solution1(data: list):
-    matrix = Matrix()
-
-    for c in data:
-        if c.is_horizontal:
-            matrix.expand(c)
-            for x in range(c.start_x, c.end_x + 1):
-                matrix.m[c.start_y][x] += 1
-        elif c.is_vertical:
-            matrix.expand(c)
-            for y in range(c.start_y, c.end_y + 1):
-                matrix.m[y][c.start_x] += 1
-    
-    return sum(1 for y in matrix.m for x in y if x > 1)
-
-
-print(f'AoC [Day 05][Part 1] Solution: {solution1(data)}')
+print(f'AoC [Day 03][Part 1] Solution: {solution1(False)}')
+print(f'AoC [Day 03][Part 2] Solution: {solution1(True)}')
