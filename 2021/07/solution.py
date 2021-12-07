13d10bda504b89425f05fe103aaa746c2e92d134
Gökhan Öztürk <gokhan@eg-ozturk.com>
2021-12-07 10:22:38 +0300

    adds 2021 day7 solutions

--- 2021/07/solution.py
+++ 2021/07/solution.py
@@ -1,32 +1,30 @@
-solution = """#!/usr/bin/env python
+#!/usr/bin/env python
 
 class Solution:
-    year = <year>
-    day = <day>
+    year = 2021
+    day = 7
     input: str
     data: list
-    
+
     def __init__(self):
         with open('./input.txt') as f:
             self.input = f.read()
         
         self.prepare_data()
     
-
     def prepare_data(self):
-        pass
-
+        self.data = [*map(int, self.input.split(","))]
 
     def part1(self):
-        pass
+        return min([sum([abs(targetPos - crab) for crab in self.data]) for targetPos in range(max(*self.data))])
 
 
     def part2(self):
-        pass
+        f = lambda n: n * (n + 1) // 2
+        return min([sum([f(abs(targetPos - crab)) for crab in self.data]) for targetPos in range(max(*self.data))])
 
 
 if __name__ == '__main__':
     s = Solution()
     print(f'AoC [{s.year} - Day {s.day}] Part 1: {s.part1()}')
     print(f'AoC [{s.year} - Day {s.day}] Part 2: {s.part2()}')
-"""
--- aoc.py
+++ aoc.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import argparse
 from os import error, mkdir
 import os
