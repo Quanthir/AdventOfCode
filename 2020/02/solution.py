e6a3626a0e04d4856438a00ff113427a5b5fc531
Gökhan Öztürk <gokhan@eg-ozturk.com>
2021-12-10 12:58:16 +0300

    updated 2020 day 3 solution

--- 2020/02/solution.py
+++ 2020/02/solution.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 class Solution:
     year = 2020
     day = 2
@@ -13,15 +15,17 @@ class Solution:
 
 
     def prepare_data(self):
-        pass
+        data = [re.match(r'(\d+)-(\d+) (\w): (\w+)', x).groups() for x in self.input.split("\n")]
+        self.data = [[int(x[0]), int(x[1]), x[2], x[3]] for x in data]
 
 
     def part1(self):
-        pass
+        return sum(x[0] <= x[3].count(x[2]) <= x[1] for x in self.data)
 
 
     def part2(self):
-        pass
+        puz = lambda p1, p2, c, s: (s[p1] == c or s[p2] == c) and s[p1] != s[p2]
+        return sum(puz(x[0]-1, x[1]-1, x[2], x[3]) for x in self.data)
 
 
 if __name__ == '__main__':
