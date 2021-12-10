6e5ff7ffb3419f322f6ca577ba61e2534c9ec784
Gökhan Öztürk <gokhan@eg-ozturk.com>
2021-12-10 10:06:35 +0300

    Updated 2021/10 challenge.txt

---
+++ 2021/04/solution.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+class Solution:
+    year = 2021
+    day = 4
+    input: str
+    data: list
+    
+    def __init__(self):
+        with open('./input.txt') as f:
+            self.input = f.read()
+        self.prepare_data()
+
+
+    def prepare_data(self):
+        pass
+
+
+    def part1(self):
+        pass
+
+
+    def part2(self):
+        pass
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(f'AoC [{s.year} - Day {s.day}] Part 1: {s.part1()}')
+    print(f'AoC [{s.year} - Day {s.day}] Part 2: {s.part2()}')
