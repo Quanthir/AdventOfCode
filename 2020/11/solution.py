d54828484ae1bbaa978e91a7333e4582cb4148df
Quanthir <gokhan@eg-ozturk.com>
2021-12-13 23:42:05 +0300

    adds 2020 day 11 part 1 solution

--- 2020/11/solution.py
+++ 2020/11/solution.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+from pprint import pprint
+from typing import ChainMap
+
+
 class Solution:
     year = 2020
     day = 11
@@ -12,44 +16,49 @@ class Solution:
         self.prepare_data()
 
     def prepare_data(self):
-        # first we need to determine the grid max dimensions.
-        grid = [[seat for seat in line] for line in self.input.split("\n")]
-        # Then need to create a coordinate map (which will be our main data)
-        # we will ignore the dots since they have no value for the puzzle
-        self.data = {
-            (x, y): 0
-            for y, row in enumerate(grid)
-                for x, seat in enumerate(row) if seat == 'L'
-        }
+        self.data = self.input.split("\n")
     
-    def neighbours(self, x, y):
-        """ Gives the adjacent seat coordinates for the given coordinate
-        """
-        return [
-            adj
-            for adj in self.data.keys()
-            if adj in [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
-        ]
+    def adjacents(self, data, x, y):
+        adj = []
+        for dx in (x-1,x,x+1):
+            for dy in (y-1,y,y+1):
+                if (dx, dy) == (x, y):
+                    continue
+                if 0 <= dx < len(data) and 0 <= dy < len(data[0]):
+                    adj.append(data[dx][dy])
+        return adj
+
+    def round(self, data):
+        grid = ['' for _ in range(len(data))]
+        changed = False
+        for x, row in enumerate(data):
+            for y, v in enumerate(row):
+                if v == '.':
+                    grid[x] += '.'
+                    continue
+                
+                adj = ''.join(self.adjacents(data, x, y))
+                if v == 'L':
+                    if adj.count('#') == 0:
+                        grid[x] += '#'
+                        changed = True
+                    else:
+                        grid[x] += 'L'
+                elif v == '#':
+                    if adj.count('#') >= 4:
+                        grid[x] += 'L'
+                        changed = True
+                    else:
+                        grid[x] += '#'
+        return changed, grid
 
     def part1(self):
-        # We need to loop all coordinates and apply the given rules to each seat
-        # depending of their situations.
-        # this is just one round. We need to loop it until no changes occur
+        grid = self.data
         changed = True
         while changed:
-            changed = False
-            for coord, seat in self.data.items():
-                neighbours = self.neighbours(*coord)
-                if seat == 0 and all(self.data[s] == 0 for s in neighbours):
-                    # if all seats in neighbours is empty, make the seat occupied
-                    self.data[coord] = 1
-                    changed = True
-                elif seat == 1 and sum(self.data[s] == 1 for s in neighbours) > 3:
-                    # if 4 or more seats in neighbours occupied, empty it
-                    self.data[coord] = 0
-                    changed = True
-        
-        return sum(seat for seat in self.data.values() if seat == 1)
+            changed, grid = self.round(grid)
+
+        return ''.join(grid).count('#')
 
     def part2(self):
         pass
