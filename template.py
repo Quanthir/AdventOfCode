4b9a41a1691ceb85e2b5256963690a482e7bbf4a
goztrk <me@gokhan.org.tr>
2023-12-02 06:17:48 +0300

    Update template file
    - Change single quotes to double quote to satisfy black
    - Remove `pass` and add `return`
    - Automatically split the input file into lines

--- template.py
+++ template.py
@@ -6,23 +6,23 @@ class Solution:
     input: str
     data: list
 
-    def __init__(self, folder='.'):
-        with open(f'{folder}/input.txt') as f:
+    def __init__(self, folder="."):
+        with open(f"{folder}/input.txt") as f:
             self.input = f.read()
         self.prepare_data()
 
     def prepare_data(self):
-        pass
+        self.data = self.input.split("\n")
 
     def part1(self):
-        pass
+        return "Unknown"
 
     def part2(self):
-        pass
+        return "Unknown"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     s = Solution()
-    print(f'AoC [{s.year} - Day {s.day}] Part 1: {s.part1()}')
-    print(f'AoC [{s.year} - Day {s.day}] Part 2: {s.part2()}')
+    print(f"AoC [{s.year} - Day {s.day}] Part 1: {s.part1()}")
+    print(f"AoC [{s.year} - Day {s.day}] Part 2: {s.part2()}")
 """
