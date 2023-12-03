d48976e96dd0c7b334fa151ac8efe360a2a4072d
goztrk <me@gokhan.org.tr>
2023-12-03 16:15:04 +0300

    Update styling and wordings on 2023/03

--- 2023/03/solution.py
+++ 2023/03/solution.py
@@ -25,9 +25,11 @@ class Solution:
             end = m.end() + 1
             yield start, min(end, len(line) - 1), int(m[0])
 
-    def has_symbol(self, y, start, end, check):
-        """Checks if the number has any symbols adjacent to it"""
+    def get_coords(self, y, start, end, check):
+        """Get the coords if the number has any symbols adjacent to it"""
         return [
+            # x is index number of sliced part so need to add ``start`` to
+            # find the correct ``x`` coord.
             f"{x + start}-{ln}"
             for ln in [max(y - 1, 0), y, min(y + 1, len(self.data) - 1)]
             for x, char in enumerate(self.data[ln][start:end])
@@ -35,40 +37,43 @@ class Solution:
         ]
 
     def part1(self):
-        """Part 1 asks for sum of all numbers near any symbol except ``.``"""
+        """Part 1
+
+        Sum of all numbers near any symbol except ``.``
+        """
         return sum(
             number
             for y, line in enumerate(self.data)
             for start, end, number in self.find_numbers(line)
-            if any(self.has_symbol(y, start, end, lambda c: c not in "1234567890."))
+            if any(self.get_coords(y, start, end, lambda c: c not in "1234567890."))
         )
 
     def part2(self):
         """Part 2
-        Find the 2 number adjacent to ``*`` and multiply them and give the sum
+
+        Find the 2 number adjacent to a ``*`` and multiply them.
+        Solution is the result of those.
         """
         # Find the numbers with ``*`` near to them
         numbers = [
             (number, coord)
             for y, line in enumerate(self.data)
             for start, end, number in self.find_numbers(line)
-            for coord in self.has_symbol(y, start, end, lambda c: c == "*")
+            for coord in self.get_coords(y, start, end, lambda c: c == "*")
         ]
 
         checked = []
 
-        def append(c, number):
-            checked.append(c)
+        def append(coord, number):
+            checked.append(coord)
             return number
 
-        result = [
-            append(c1, n1 * n2)
-            for i, (n1, c1) in enumerate(numbers)
-            for j, (n2, c2) in enumerate(numbers)
-            if j != i and c1 == c2 and c1 not in checked
-        ]
-
-        return sum(result)
+        return sum(
+            append(coord1, number1 * number2)
+            for i, (number1, coord1) in enumerate(numbers)
+            for j, (number2, coord2) in enumerate(numbers)
+            if j != i and coord1 == coord2 and coord1 not in checked
+        )
 
 
 if __name__ == "__main__":
