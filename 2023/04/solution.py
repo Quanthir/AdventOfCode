d2ae8ea8126e3a1ff7c148f9b60c3c9950497549
goztrk <me@gokhan.org.tr>
2023-12-04 18:01:43 +0300

    Shorten 2023/04 solution

--- 2023/04/solution.py
+++ 2023/04/solution.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import re
-
 
 class Solution:
     year = 2023
@@ -10,41 +8,27 @@ class Solution:
 
     def __init__(self, folder="."):
         with open(f"{folder}/input.txt") as f:
-            self.input = f.read()
+            self.input = f.read().strip().split("\n")
         self.prepare_data()
 
     def prepare_data(self):
-        self.data = [
-            len(
-                [
-                    1
-                    for _ in set(map(lambda x: int(x), left.split()))
-                    & set(map(lambda x: int(x), right.split()))
-                ]
-            )
-            for line in self.input.strip().split("\n")
-            for left, right in [re.sub(r"Card +\d+: ", "", line).split(" | ")]
-        ]
+        self.solution1 = 0
+        self.solution2 = [1] * len(self.input)
+
+        for i, line in enumerate(self.input):
+            left, right = line[10:].split(" | ")
+
+            if (count := len(set(left.split()) & set(right.split()))):
+                self.solution1 += 2 ** (count - 1)
+
+            for j in range(count):
+                self.solution2[i + j + 1] += self.solution2[i]
 
     def part1(self):
-        return sum(
-            2 ** (count - 1)
-            for count in self.data
-            if count > 0
-        )
+        return self.solution1
 
     def part2(self):
-        result = [1] * len(self.data)
-        generator = (
-            (j, result[i])
-            for i, count in enumerate(self.data)
-            if count > 0
-            for j in range(i + 1, i + 1 + count)
-        )
-        for i, copies in generator:
-            result[i] += copies
-
-        return sum(result)
+        return sum(self.solution2)
 
 
 if __name__ == "__main__":
